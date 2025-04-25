cec40393f22f3a4604edda47e72544697727768e
CyrilH714 <cyril.j.haller@gmail.com>
2025-04-25 21:40:09 +0100

    Add state variables. Change CSS layout to grid, incomplete

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*
+THEME: DINO DIGGER
+-Want to hide our dino fossils from rival paleontologist
+-Take turns with rival to guess their fossil location on site/grid
+-Correct guess means go again. Boobytrap means dig on your own site/grid
+*/
+
+
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+// My Board. Array of arrays. Nested arrays represent columns
+// 1/-1 for player in position, null for empty
+
+
+// rival Board. Array of arrays. Nested arrays represent columns
+// 1/-1 for player in position, null for empty
+
+// Winner. values: null=no winner/no tie, in progress,
+//  1/-1=winner, "Draw"= tied game
+
+// Turn. 1/-1 for turn
+
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
