1d4477f8128f4e0bb3fa8b2e41b7d3c0189ccb07
CyrilH714 <cyril.j.haller@gmail.com>
2025-04-26 16:43:32 +0100

    CSS troubleshooting

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /*
 THEME: DINO DIGGER
 -Want to hide our dino fossils from rival paleontologist
+-Choose where to bury fossils (can add rotation option later).
+-Randomly generate where enemy buries theirs (hide from user)
 -Take turns with rival to guess their fossil location on site/grid
 -Correct guess means go again. Boobytrap means dig on your own site/grid
 */
@@ -13,16 +15,21 @@ THEME: DINO DIGGER
 /*----- state variables -----*/
 // My Board. Array of arrays. Nested arrays represent columns
 // 1/-1 for player in position, null for empty
-
+let myBoard;
 
 // rival Board. Array of arrays. Nested arrays represent columns
 // 1/-1 for player in position, null for empty
+let rivalBoard;
 
 // Winner. values: null=no winner/no tie, in progress,
 //  1/-1=winner, "Draw"= tied game
+let winner;
 
 // Turn. 1/-1 for turn
+let turn;
 
+// fossils. 1/-1 for intact/dug. null for not on board yet.
+let fossils;
 
 
 /*----- cached elements  -----*/
