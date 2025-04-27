d820f305d0eda156bebcc2b96c13350a613c0b69
CyrilH714 <cyril.j.haller@gmail.com>
2025-04-27 17:31:11 +0100

    Write first init and render functions including render my board and rival board. Assign colours to cells

--- js/main.js
+++ js/main.js
@@ -10,11 +10,16 @@ THEME: DINO DIGGER
 
 
 /*----- constants -----*/
-
+const COLOURS ={
+    "1": "orange",
+    "-1": "white",
+    "null" : "none",
+    "0": "black",
+}
 
 /*----- state variables -----*/
 // My Board. Array of arrays. Nested arrays represent columns
-// 1/-1 for player in position, null for empty
+// 1 for player in position, -1 for dug position, null for empty
 let myBoard;
 
 // rival Board. Array of arrays. Nested arrays represent columns
@@ -25,7 +30,7 @@ let rivalBoard;
 //  1/-1=winner, "Draw"= tied game
 let winner;
 
-// Turn. 1/-1 for turn
+// Turn. 1/-1 for turn. null for initial stage of placing pieces
 let turn;
 
 // fossils. 1/-1 for intact/dug. null for not on board yet.
@@ -39,3 +44,67 @@ let fossils;
 
 
 /*----- functions -----*/
+init() 
+
+function init(){
+    winner=null;
+    turn=null;
+    myBoard=[
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+    ];
+    rivalBoard=[
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+    ]
+        
+
+
+    render();
+
+}
+
+function render(){
+    renderMyBoard();
+    renderRivalBoard();
+    // renderMessage();
+    // renderControls();
+}
+
+function renderMyBoard(){
+    myBoard.forEach((colArr,colIdx)=>{
+        colArr.forEach((cellVal,rowIdx)=>{
+            const cellEl=document.getElementById(`c${colIdx}r${rowIdx}`)
+            cellEl.style.backgroundColor=COLOURS[cellVal];
+        })
+       
+
+    })
+}
+
+function renderRivalBoard(){
+    rivalBoard.forEach((colArr,colIdx)=>{
+        colArr.forEach((cellVal,rowIdx)=>{
+            const cellEl=document.getElementById(`cc${colIdx}r${rowIdx}`)
+            cellEl.style.backgroundColor=COLOURS[cellVal];
+        })
+       
+
+    })
+}
